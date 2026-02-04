91e1f197931a4e386ea11a9d66b5282af2c35c5a
iszkq <167393945+iszkq@users.noreply.github.com>
2026-02-04 23:35:35 +0800

    Update vite.config.ts

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,10 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { defineHaloUiPluginConfig } from '@halo-dev/ui-plugin-bundler-kit'
+import { viteConfig } from '@halo-dev/ui-plugin-bundler-kit'
 
-export default defineConfig(
-  defineHaloUiPluginConfig({
+export default viteConfig({
+  vite: {
     plugins: [vue()],
     resolve: {
       alias: {
@@ -12,15 +11,8 @@ export default defineConfig(
       },
     },
     build: {
-      lib: {
-        entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
-        formats: ['es'],
-        fileName: () => 'index.js',
-      },
-      rollupOptions: {
-        external: ['vue', '@halo-dev/components', '@halo-dev/console-shared'],
-      },
+      outDir: 'build/dist',
     },
-  })
-)
+  },
+})
 
